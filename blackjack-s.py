e6aa90033aaf3d691c04daee6bcd724d323a371c
김규빈 <inkblot626@gmail.com>
2024-10-21 01:04:30 +0900

    add while loop

--- blackjack-s.py
+++ blackjack-s.py
@@ -6,14 +6,9 @@ def deal_card():
     card = random.choice(cards)
     return card
 
-user_cards = []
-computer_cards = []
-
-for _ in range(2):
-    user_cards.append(deal_card())
-    computer_cards.append(deal_card())
 
 def calculate_score(cards):
+    """Take a list of cards and return the score calculated from the cards"""
     if sum(cards) == 21 and len(cards) == 2:
         return 0
     if 11 in cards and sum(cards) > 21:
@@ -22,3 +17,35 @@ def calculate_score(cards):
         
     return sum(cards)
 
+
+
+user_cards = []
+computer_cards = []
+computer_score = -1
+user_score = -1
+is_game_over = False
+
+for _ in range(2):
+    user_cards.append(deal_card())
+    computer_cards.append(deal_card())
+
+
+while not is_game_over:
+    user_score = calculate_score(user_cards)
+    computer_score = calculate_score(computer_cards)
+    print(f"Your cards: {user_cards}, Current Score: {user_score}")
+    print(f"Computer's first card: {computer_cards[0]}")
+
+
+    if user_score == 0 or computer_score == 0 or user_score > 21:
+        is_game_over = True
+    else:
+        user_should_deal = input("Type 'y' to get another card, type 'n' to pass: ")
+        if user_should_deal == 'y':
+            user_cards.append(deal_card())
+        else:
+            is_game_over = True
+
+while computer_score != 0 and computer_score < 17:
+    computer_cards.append(deal_card())
+    computer_score = calculate_score(computer_cards)
