1ddf430264e6320a82219614998982d333c8786c
김규빈 <inkblot626@gmail.com>
2024-10-25 23:51:40 +0900

    intro guessing game

---
+++ guessing.py
@@ -0,0 +1,2 @@
+print("Welcome to the Number Guessing Game!\nI'm thinking of a number between 1 and 100.\nPssst, the correct answer is 100")
+type = input("Choose a difficulty. Type 'easy' or 'hard': ")
