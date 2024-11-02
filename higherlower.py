4e0ba00936b3b605a96e2e568ba4df8c99416f29
김규빈 <inkblot626@gmail.com>
2024-11-02 23:39:55 +0900

    add comments

---
+++ higherlower.py
@@ -0,0 +1,11 @@
+from data import game_data
+import random
+
+
+# create two slots and put random data into them
+
+# let user choose which one has more followers
+
+# if the user was wrong, print the result and let user know their final score
+
+# if the user wins, print the next question
