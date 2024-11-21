6a2e10f82092581f1e7af428d1494a3f8a9a5cbb
김규빈 <inkblot626@gmail.com>
2024-11-21 23:48:01 +0900

    print a and b

--- higerlower-s.py
+++ higerlower-s.py
@@ -1,12 +1,25 @@
 from game_data import data
 import random
 
+
+def format_data(account):
+    """ Format the account data into printable format. """
+    account_name = account["name"]
+    account_descr = account["description"]
+    account_country = account["country"]
+    return f"{account_name}, a {account_descr}, from {account_country}"
+
+
 # Generate a ramdom account from the game data
 account_a = random.choice(data)
 account_b = random.choice(data)
 
+if account_a == account_b:
+    account_b = random.choice(data)
 
-# Format the account data into printable format.
+print(f"Compare A: {format_data(account_a).")
+print(f"Compare B: {format_data(account_b)}.")
+
 
 # Ask user for a guess.
 
