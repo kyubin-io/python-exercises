80ac847b8474d8d43ef767a6c513c2f8f0dadf34
김규빈 <inkblot626@gmail.com>
2025-04-21 23:07:53 +0900

    create coffeeMuchine

---
+++ coffee.py
@@ -0,0 +1,16 @@
+
+from coffee import MENU
+
+
+coffee = input("What would you like? (espresso/latte/cappuccino): ")
+
+print("Please insert coins.")
+
+quarters = int(input("How many quarters?: "))
+dimes = int(input("How many dimes?: "))
+nickles = int(input("How many nickles?: "))
+pennies = int(input("How many pennies?: "))
+
+sum = quarters + dimes + nickles + pennies
+
+# if sum < 
