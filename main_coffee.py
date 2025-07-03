94a1b6e2914134c820a39930455fb67c5cb3338b
김규빈 <inkblot626@gmail.com>
2025-07-03 23:35:07 +0900

    day-17 setup

--- main_coffee.py
+++ main_coffee.py
@@ -1,26 +1,26 @@
-from menu import Menu
+from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
+coffee_maker = CoffeeMaker()
+money_machine = MoneyMachine()
 menu = Menu()
-coffeeMaker = CoffeeMaker()
-moneyMachine = MoneyMachine()
-
 
 is_on = True
-profit = 0
+
+
+
 
 while is_on:
-    choice = input("What would you like? (espresso/latte/cappuccino): ")
+    options = menu.get_items()
+    choice = input(f"What would you like? : {options}")
     if choice == "off":
         is_on = False
     elif choice == "report":
-        print(coffeeMaker.report())
-        print(f"Money : ${profit}")
+        coffee_maker.report()
+        money_machine.report()
     else:
-        drink = menu(choice)
-        if coffeeMaker.is_resource_sufficient(drink["ingredients"]):
-            payment = moneyMachine.process_coins()
-            if coffeeMaker.is_transaction_successful(payment, drink["cost"]):
-                coffeeMaker.make_coffee(choice, drink["ingredients"])
+        drink = menu.find_drink(choice)
+        if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
+                coffee_maker.make_coffee(drink)
 
