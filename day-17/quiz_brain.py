cc92d6abfb9a32083d87ffcdcbd6dea3edc8d3b3
김규빈 <inkblot626@gmail.com>
2025-07-05 01:46:06 +0900

    add final print

--- day-17/quiz_brain.py
+++ day-17/quiz_brain.py
@@ -13,9 +13,13 @@ class QuizBrain:
         user_answer = input(f"Q.{self.question_number}: {current_question.text} (True/False)?")
         self.check_answer(user_answer, current_question.answer)
 
+
     def check_answer(self, user_answer, correct_answer):
         if user_answer.lower() == correct_answer.lower():
             print("You got it right!")
+            self.score += 1            
         else:
             print("That's wrong.")
         print(f"The correct answer was: {correct_answer}.")
+        print(f"Your current score is: {self.score}/{self.question_number}")
+        print("\n")
