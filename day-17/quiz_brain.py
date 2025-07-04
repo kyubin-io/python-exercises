1dc1fcadfd6eda47c14293eb07d8e95861ce410d
김규빈 <inkblot626@gmail.com>
2025-07-05 00:59:00 +0900

    how to shorten the code removing if

--- day-17/quiz_brain.py
+++ day-17/quiz_brain.py
@@ -2,8 +2,11 @@ class QuizBrain:
     def __init__(self, q_bank):
         self.question_number = 0
         self.question_list = q_bank
+
+    def still_has_questions(self):
+        return self.question_number < len(self.question_list)
     
     def next_question(self):
         current_question = self.question_list[self.question_number]
         self.question_number += 1
-        answer = input(f"Q.{self.question_number}: {current_question.text} (True/False)?")
+        input(f"Q.{self.question_number}: {current_question.text} (True/False)?")
