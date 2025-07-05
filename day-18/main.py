3e6c5723639b409feae90920b7f2d31302bf55d1
김규빈 <inkblot626@gmail.com>
2025-07-05 16:09:56 +0900

    random movement

--- day-18/main.py
+++ day-18/main.py
@@ -5,7 +5,7 @@ tim = Turtle()
 tim.shape("turtle")
 tim.color("green")
 
-colours = ["CornflowerBlue", "DarkOrchid", "IndianRed", "LightSeaGreen" , "Wheat", "SlateGray", "SeaGreen"]
+colours = ["CornflowerBlue", "DarkOrchid", "IndianRed", "LightSeaGreen", "Wheat", "SlateGray", "SeaGreen", "FloralWhite", "LightCoral", "SkyBlue", "CadetBlue", "Crimson"]
 
 # for _ in range(4):
 #     tim.forward(100)
@@ -19,15 +19,25 @@ colours = ["CornflowerBlue", "DarkOrchid", "IndianRed", "LightSeaGreen" , "Wheat
 #     tim.pendown()
 
 
-def draw_shape(num_sides):
-    angle = 360 / num_sides
-    for _ in range(num_sides):
-        tim.forward(100)
-        tim.left(angle)
+# def draw_shape(num_sides):
+#     angle = 360 / num_sides
+#     for _ in range(num_sides):
+#         tim.forward(100)
+#         tim.left(angle)
 
-for shape_side_n in range(3, 11):
+# for shape_side_n in range(3, 11):
+#     tim.color(random.choice(colours))
+#     draw_shape(shape_side_n)
+
+directions = [0, 90, 180, 270]
+tim.pensize(15)
+tim.speed("fastest")
+
+for _ in range(200):
     tim.color(random.choice(colours))
-    draw_shape(shape_side_n)
+    tim.forward(30)
+    tim.setheading(random.choice(directions))
+
 
 screen = Screen()
 screen.exitonclick()
