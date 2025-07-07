b20204af9178d71d630257152f982820b5c53a18
김규빈 <inkblot626@gmail.com>
2025-07-07 23:47:59 +0900

    add color_list

--- day-18/main.py
+++ day-18/main.py
@@ -1,11 +1,20 @@
-from turtle import Turtle, Screen
+# from turtle import Turtle, Screen
+import turtle as t
 import random
 
-tim = Turtle()
+tim = t.Turtle()
 tim.shape("turtle")
 tim.color("green")
+t.colormode(255)
 
-colours = ["CornflowerBlue", "DarkOrchid", "IndianRed", "LightSeaGreen", "Wheat", "SlateGray", "SeaGreen", "FloralWhite", "LightCoral", "SkyBlue", "CadetBlue", "Crimson"]
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    return (r, g, b)
+
+
+# colours = ["CornflowerBlue", "DarkOrchid", "IndianRed", "LightSeaGreen", "Wheat", "SlateGray", "SeaGreen", "FloralWhite", "LightCoral", "SkyBlue", "CadetBlue", "Crimson"]
 
 # for _ in range(4):
 #     tim.forward(100)
@@ -30,14 +39,23 @@ colours = ["CornflowerBlue", "DarkOrchid", "IndianRed", "LightSeaGreen", "Wheat"
 #     draw_shape(shape_side_n)
 
 directions = [0, 90, 180, 270]
-tim.pensize(15)
+# tim.pensize(15)
 tim.speed("fastest")
 
-for _ in range(200):
-    tim.color(random.choice(colours))
-    tim.forward(30)
-    tim.setheading(random.choice(directions))
+# for _ in range(200):
+#     tim.color(random_color())
+#     tim.forward(30)
+#     tim.setheading(random.choice(directions))
 
 
-screen = Screen()
+def draw_spirograph(size_of_gap):
+    for _ in range(int(360 / size_of_gap)):
+        tim.color(random_color())
+        tim.circle(100)
+        tim.setheading(tim.heading() + size_of_gap)
+
+
+draw_spirograph(5)
+
+screen = t.Screen()
 screen.exitonclick()
