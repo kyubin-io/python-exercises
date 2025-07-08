649a7f6be59d46d9ea3689077f05a6e34d504f14
김규빈 <inkblot626@gmail.com>
2025-07-08 22:53:30 +0900

    no need to pendown when drawing dot

--- day-18/hirst-painting/main.py
+++ day-18/hirst-painting/main.py
@@ -1,36 +1,38 @@
-from turtle import Turtle, Screen
+import turtle as turtle_module
 import random
 
-tim = Turtle()
-tim.shape("turtle")
-tim.color("green")
 
-screen = Screen()
-screen.colormode(255)
-
+turtle_module.colormode(255)
+tim = turtle_module.Turtle()
+tim.speed("fastest")
+tim.penup()
 color_list = [(202, 164, 109), (238, 240, 245), (150, 75, 49), (223, 201, 135), (52, 93, 124), (172, 154, 40), (140, 30, 19), (133, 163, 185), (198, 91, 71), (46, 122, 86), (72, 43, 35), (145, 178, 148), (13, 99, 71), (233, 175, 164), (161, 142, 158), (105, 74, 77), (55, 46, 50), (183, 205, 171), (36, 60, 74), (18, 86, 90), (81, 148, 129), (148, 17, 20), (14, 70, 64), (30, 68, 100), (107, 127, 153), (174, 94, 97), (176, 192, 209)]
 
-tim.penup()
-x_coord = -screen.window_width() / 2 + 20 # Add a small offset from the edge
-y_coord = -screen.window_height() / 2 + 20 # Add a small offset from the edge
+tim.setheading(225)
+tim.forward(300)
+tim.setheading(0)
 
-tim.goto(x_coord, y_coord)
+number_of_dots = 100
 
-for _ in range(10):
-    tim.pendown()
-    for _ in range(10):
-        tim.pencolor(random.choice(color_list))
-        tim.dot(20)
-        tim.penup()
+for dot_count in range(1, number_of_dots + 1):
+    tim.dot(20, random.choice(color_list))
+    tim.forward(50)
+
+    if dot_count % 10 == 0:
+        tim.setheading(90)
         tim.forward(50)
-        tim.pendown()
-    tim.penup()
-    y_coord += 40
-    tim.goto(x_coord, y_coord)
-    
+        tim.setheading(180)
+        tim.forward(500)
+        tim.setheading(0)
 
 
 
+
+
+screen = turtle_module.Screen()
 screen.exitonclick()
+
+
+
 
 
