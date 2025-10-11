07d8ab4a30e0d97fd90aa22e79e5ad3ed1094771
김규빈 <inkblot626@gmail.com>
2025-10-11 23:56:16 +0900

    add scoreboard

--- day-20/main.py
+++ day-20/main.py
@@ -1,6 +1,7 @@
 from turtle import Screen
 from snake import Snake
 from food import Food
+from scoreboard import Scoreboard
 import time
 
 
@@ -13,6 +14,7 @@ screen.tracer(0)
 
 snake = Snake()
 food = Food()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -27,6 +29,11 @@ while game_is_on:
     time.sleep(0.1)
 
     snake.move()
+
+    # detect collision with food.
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        scoreboard.increase_score()
 
     
 
