0df204e077638d9f3195fd5ea6bc2a6522739e97
김규빈 <inkblot626@gmail.com>
2025-10-11 23:35:21 +0900

    import food

--- day-20/main.py
+++ day-20/main.py
@@ -1,5 +1,6 @@
 from turtle import Screen, Turtle
 from snake import Snake
+from food import Food
 import time
 
 
@@ -11,6 +12,7 @@ screen.title("My Snake Game")
 screen.tracer(0)
 
 snake = Snake()
+food = Food()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
