a4c47499622137bd63941929b66ca51214d80392
김규빈 <inkblot626@gmail.com>
2025-10-14 00:57:09 +0900

    detect collision with car

--- day-23/main.py
+++ day-23/main.py
@@ -1,4 +1,7 @@
 from turtle import Screen
+from player import Player
+from car_manager import CarManager
+from scoreboard import Scoreboard
 import time
 
 
@@ -8,9 +11,12 @@ screen.setup(width=600, height=600)
 screen.title("Turtle Crossing")
 screen.tracer(0)
 
+player = Player()
+car_manager = CarManager()
 
 screen.listen()
 
+screen.onkey(player.go_up, "Up")
 
 game_is_on = True
 
@@ -18,6 +24,13 @@ while game_is_on:
     time.sleep(0.1)
     screen.update()
 
+    car_manager.create_car()
+    car_manager.move_cars()
+
+    # detect collision with car
+    for car in car_manager.all_cars:
+        if car.distance(player) < 20:
+            game_is_on = False
 
 
 screen.exitonclick()
