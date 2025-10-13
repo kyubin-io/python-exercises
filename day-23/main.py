765db6500509d6a2e0c56ffb4972387856b0f565
김규빈 <inkblot626@gmail.com>
2025-10-14 01:09:32 +0900

    add scoreboard

--- day-23/main.py
+++ day-23/main.py
@@ -13,9 +13,9 @@ screen.tracer(0)
 
 player = Player()
 car_manager = CarManager()
+scoreboard = Scoreboard()
 
 screen.listen()
-
 screen.onkey(player.go_up, "Up")
 
 game_is_on = True
@@ -31,6 +31,15 @@ while game_is_on:
     for car in car_manager.all_cars:
         if car.distance(player) < 20:
             game_is_on = False
+            scoreboard.game_over()
+
+
+    # detect successful crossing
+    if player.is_at_finish_line():
+        player.go_to_start()
+        car_manager.level_up()
+        scoreboard.increase_level()
+
 
 
 screen.exitonclick()
