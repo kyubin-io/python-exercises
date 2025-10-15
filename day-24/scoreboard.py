08204083225aa4ad744ce9c2a65c6f9777b17e24
김규빈 <inkblot626@gmail.com>
2025-10-15 22:33:38 +0900

    reset indstead game over

--- day-24/scoreboard.py
+++ day-24/scoreboard.py
@@ -16,11 +16,13 @@ class Scoreboard(Turtle):
         self.update_scoreboard()
 
     def update_scoreboard(self):
-        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+        self.write(f"Score: {self.score} High Score: {self.high_score}", align=ALIGNMENT, font=FONT)
 
     def reset(self):
         if self.score > self.high_score:
             self.high_score = self.score
+        self.score = 0
+        self.update_scoreboard()  
 
     def increase_score(self):
         self.score += 1
